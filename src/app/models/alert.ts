60649d471b4dc85106ffaf2d4c9b4e139626b82f
Prabesh Niraula <prabshniraula@gmail.com>
2018-06-07 19:20:10 -0400

    Added model and service for alert

---
+++ src/app/models/alert.ts
@@ -0,0 +1,14 @@
+export class Alert{
+    public AlertId:number;
+    public AlertTime:string;
+    public Severity:string;
+    public ClientIP:string;
+    public ServerIP:string;
+    public Protocal:string;
+    public ClientCountry:string;
+    constructor(props:any){
+        Object.keys(props).forEach(key=>{
+            this[props] = props[key];
+        });
+    }
+}
---
+++ src/app/services/alerts.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, inject } from "@angular/core";
+import { Http } from "@angular/http";
+
+import { Alert } from "../models/alert";
+@Injectable()
+export class AlertsService{
+
+    constructor(private http:Http){
+
+    }
+    
+    public getAlerts(){
+        return this.http.get('../data/alert.json');
+    }
+}
